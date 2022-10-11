05418674ed0f5e71cbdf60080049ac6f4a70d357
Stharley Maxwell <stharleymax@gmail.com>
2022-10-11 20:28:03 -0300

    add: firt resource flask

---
+++ app.py
@@ -0,0 +1,38 @@
+from flask import Flask
+from flask_restful import Resource, Api
+
+app = Flask(__name__)
+api = Api(app)
+
+hoteis = [
+    {
+        'hotel_id': 'alpha',
+        'nome': 'Alpha Hotel',
+        'estrelas': 4.3,
+        'diaria': 420.34,
+        'cidade': 'Goiás'
+    },
+    {
+        'hotel_id': 'bravo',
+        'nome': 'Bravo Hotel',
+        'estrelas': 4.1,
+        'diaria': 410.34,
+        'cidade': 'Brasília'
+    },
+    {
+        'hotel_id': 'charlie',
+        'nome': 'Charlie Hotel',
+        'estrelas': 3.9,
+        'diaria': 330.34,
+        'cidade': 'São paulo'
+    }
+]
+
+class Hoteis(Resource):
+    def get(self):
+        return {'hoteis': hoteis}
+
+api.add_resource(Hoteis, '/hoteis')
+
+if __name__ == '__main__':
+    app.run(debug=True)
