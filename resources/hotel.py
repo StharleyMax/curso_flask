4b7ad5fdeccc39d6213cf5ed32a781360dd04b35
Stharley Maxwell <stharleymax@gmail.com>
2022-10-16 15:32:56 -0300

    ADD: add handle post hotel

--- resources/hotel.py
+++ resources/hotel.py
@@ -3,8 +3,7 @@ Resource hotel
 """
 
 # External imports
-from ast import Delete
-from flask_restful import Resource
+from flask_restful import Resource, reqparse
 
 
 hoteis = [
@@ -49,16 +48,47 @@ class Hotel(Resource):
     Class Hotel
     """
 
-    def get(self, hotel_id):
-        
+    def get(self, hotel_id) -> dict:
+        """
+        Handle get hotel.
+
+        :params hotel_id: id hotel to find.
+
+        :return: hotel dict
+        """
+
         for hotel in hoteis:
             if hotel['hotel_id'] == hotel_id:
                 return hotel
 
         return {'message': 'Hotel not found'}, 404
     
-    def post(self, hotel_id):
-        pass
+    def post(self, hotel_id) -> dict:
+        """
+        Handle post hotel.
+
+        :params hotel_id: id hotel to create.
+
+        :return: hotel dict create
+        """
+
+        argumentos = reqparse.RequestParser()
+        argumentos.add_argument('nome')
+        argumentos.add_argument('estrelas')
+        argumentos.add_argument('diaria')
+        argumentos.add_argument('cidade')
+
+        dados = argumentos.parse_args()
+        novo_hotel = {
+            'hotel_id': hotel_id,
+            'nome': dados['nome'],
+            'estrelas': dados['estrelas'],
+            'diaria': dados['diaria'],
+            'cidade': dados['cidade'],
+        }
+
+        hoteis.append(novo_hotel)
+        return novo_hotel, 200
 
     def put(self, hotel_id):
         pass
