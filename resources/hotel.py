eec697fe63fa96f1cffe9fc3dd1e85a4ea4b3bc2
Stharley Maxwell <stharleymax@gmail.com>
2022-10-16 15:40:53 -0300

    update: refactor method find_hotel

--- resources/hotel.py
+++ resources/hotel.py
@@ -48,9 +48,9 @@ class Hotel(Resource):
     Class Hotel
     """
 
-    def get(self, hotel_id) -> dict:
+    def find_hotel(self, hotel_id: str) -> dict:
         """
-        Handle get hotel.
+        find hotel.
 
         :params hotel_id: id hotel to find.
 
@@ -60,10 +60,23 @@ class Hotel(Resource):
         for hotel in hoteis:
             if hotel['hotel_id'] == hotel_id:
                 return hotel
+        return None
 
-        return {'message': 'Hotel not found'}, 404
+    def get(self, hotel_id: str) -> dict:
+        """
+        Handle get hotel.
+
+        :params hotel_id: id hotel to find.
+
+        :return: hotel dict
+        """
+
+        hotel = self.find_hotel(hotel_id)
+        if not hotel:
+            return {'message': 'Hotel not found'}, 404
+        return hotel
     
-    def post(self, hotel_id) -> dict:
+    def post(self, hotel_id: str) -> dict:
         """
         Handle post hotel.
 
@@ -71,6 +84,9 @@ class Hotel(Resource):
 
         :return: hotel dict create
         """
+
+        if self.find_hotel(hotel_id):
+            return {'message': f'Hotel {hotel_id} already exists'}
 
         argumentos = reqparse.RequestParser()
         argumentos.add_argument('nome')
